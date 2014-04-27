3f0f18876eb3ecb8e530416e5f1e664da0b944a5
Masaaki Morishita <ikasama3.0@gmail.com>
2014-04-27 19:17:47 +0900

    fix example

--- example.js
+++ example.js
@@ -1,23 +1,7 @@
 
-var memoi = require('memoi');
-var fib = memoi.memoize(function(n) {
-  if (n < 2) {
-    return n;
-  } else {
-    return fib(n - 1) + fib(n - 2);
-  }
+var memoi = require('memoi')();
+
+var fib = memoi.ze(function(n) {
+  if (n < 2) return n;
+  else return fi (n - 1) + fib(n - 2);
 });
-
-// or
-
-var option = {storage: ['localStorage']};
-var memoi = new Memoi(option);
-var fib = memoi.memoize(function(n) {
-  if (n < 2) {
-    return n;
-  } else {
-    return fib(n - 1) + fib(n - 2);
-  }
-});
-
-memoi.get(memoi.memo['fib(10)']); // => 55
