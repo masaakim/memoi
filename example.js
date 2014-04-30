8f820c8a4fa698429eefe8261440ffb3cad90bd7
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-01 06:20:27 +0900

    fix example

--- example.js
+++ example.js
@@ -5,3 +5,10 @@ var fib = memoi.ze(function(n) {
   if (n < 2) return n;
   else return fi (n - 1) + fib(n - 2);
 });
+
+
+// -------------
+
+var memoi = require('memoi');
+var memcached = require('memo-memcached');
+memoi.ze(func).use(memcached);
