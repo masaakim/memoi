251d6658b1674bd4756cc1ec2569b5124b29de39
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-01 22:32:33 +0900

    fix example

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
 
-var memoi = require('memoi')();
+var memoi = require('./');
 
 var fibonacci = memoi(function(n) {
   return n < 2 ? n: fibonacci(n - 1) + fibonacci(n - 2);
 });
+
+fibonacci(10); // 55
