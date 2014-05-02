9d5d10c651723d7347f50645ddf804f94a150913
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-02 19:41:48 +0900

    fix test

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 var memoi = require('./');
 var test = require('tape');
 
-var fibonacci_memo = memoi(function(n) {
+var fibonacci_memo = memoi.ze(function(n) {
   return n < 2 ? n: fibonacci_memo(n - 1) + fibonacci_memo(n - 2);
 });
 
-var fibonacci = memoi(function(n) {
+var fibonacci = function(n) {
   return n < 2 ? n: fibonacci(n - 1) + fibonacci(n - 2);
-});
+}
 
 test('memoize', function(t) {
   var result = fibonacci_memo(10);
